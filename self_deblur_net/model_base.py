77b0732b4fcb65a10be4dbf09488846a36d64801
adityakrishnavamsy <46970931+adityakrishnavamsy@users.noreply.github.com>
2020-11-21 17:48:05 +0530

    Update model_base.py

--- self_deblur_net/model_base.py
+++ self_deblur_net/model_base.py
@@ -1,30 +1,30 @@
 import torch
 import os
 import numpy as np
-from torch.optim import lr_scheduler
+from torch.optim import lr_scheduler    #learning rate scheduler as optimizer 
 
 class ModelBase():
-    def save_network(self, network, network_label, epoch_label, save_dir, on_gpu=True):
-        if not os.path.exists(save_dir):
+    def save_network(self, network, network_label, epoch_label, save_dir, on_gpu=True): #save model 
+        if not os.path.exists(save_dir): #if the file is not saved then create a directory 
             os.makedirs(save_dir)
-        save_filename = '%s_net_%s.pth' % (epoch_label, network_label)
-        save_path = os.path.join(save_dir, save_filename)
-        torch.save(network.cpu().state_dict(), save_path)
+        save_filename = '%s_net_%s.pth' % (epoch_label, network_label) #path files extension 
+        save_path = os.path.join(save_dir, save_filename) #https://www.geeksforgeeks.org/python-os-path-join-method/ joins paths 
+        torch.save(network.cpu().state_dict(), save_path)#save or load entire model when CPU 
 
         if on_gpu:
-            network.cuda()
+            network.cuda() #if on GPU 
 
     def load_network(self, network, network_label, epoch_label, save_dir):
         save_filename = '%s_net_%s.pth' % (epoch_label, network_label)
-        save_path = os.path.join(save_dir, save_filename)
-        network.load_state_dict(torch.load(save_path))
+        save_path = os.path.join(save_dir, save_filename)  
+        network.load_state_dict(torch.load(save_path))#laod network 
         print('load network from ', save_path)
 
     def print_networks(self, net):
         print('---------- Networks initialized -------------')
         num_params = 0
-        for param in net.parameters():
-            num_params += param.numel()
+        for param in net.parameters(): #learnable parameters of a model are returned by
+            num_params += param.numel()#number of parameters 
         print(net)
         print('[Network] Total number of parameters : %.3f M' % (num_params / 1e6))
         print('-----------------------------------------------')
@@ -32,7 +32,7 @@ class ModelBase():
     # used in test time, wrapping `forward` in no_grad() so we don't save
     # intermediate steps for backprop
     def eval(self):
-        with torch.no_grad():
+        with torch.no_grad():#Disabling gradient calculation is useful for inference, when you are sure that you will not call Tensor.backward()
             return self.forward()
 
     def build_lr_scheduler(self):
