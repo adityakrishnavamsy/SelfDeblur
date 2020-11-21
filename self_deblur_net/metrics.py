87d6f520d46aa8e29fc2b875c1776ed80a5e4f0e
adityakrishnavamsy <46970931+adityakrishnavamsy@users.noreply.github.com>
2020-11-21 18:39:32 +0530

    Update metrics.py

--- self_deblur_net/metrics.py
+++ self_deblur_net/metrics.py
@@ -5,17 +5,20 @@ from torch.autograd import Variable
 import numpy as np
 from math import exp
 
-def gaussian(window_size, sigma):
-	gauss = torch.Tensor([exp(-(x - window_size/2)**2/float(2*sigma**2)) for x in range(window_size)])
+def gaussian(window_size, sigma): # context of maximum-a-posteriori (MAP) estimation likely hood function
+	gauss = torch.Tensor([exp(-(x - window_size/2)**2/float(2*sigma**2)) for x in range(window_size)])#i think this is gauss dis function 
+	#A torch.Tensor is a multi-dimensional matrix containing elements of a single data type.
 	return gauss/gauss.sum()
 
 def create_window(window_size, channel):
-	_1D_window = gaussian(window_size, 1.5).unsqueeze(1)
-	_2D_window = _1D_window.mm(_1D_window.t()).float().unsqueeze(0).unsqueeze(0)
-	window = Variable(_2D_window.expand(channel, 1, window_size, window_size))
+	_1D_window = gaussian(window_size, 1.5).unsqueeze(1) #Returns a new tensor with a dimension of size one inserted at the specified position.
+	_2D_window = _1D_window.mm(_1D_window.t()).float().unsqueeze(0).unsqueeze(0)#Performs a matrix multiplication of the matrices input and mat2.
+	window = Variable(_2D_window.expand(channel, 1, window_size, window_size))#A Variable wraps a Tensor. It supports nearly all the API’s defined by a Tensor. 
+	#Variable also provides a backward method to perform backpropagation 
+	# tensor,requires_grad,
 	return window
 
-def SSIM(img1, img2):
+def SSIM(img1, img2):# Structural Similarity Index more it is good the image deblurred 
 	(_, channel, _, _) = img1.size()
 	window_size = 11
 	window = create_window(window_size, channel).cuda()
